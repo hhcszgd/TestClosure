8c356d485ee8307c1abfb0f2747aa39134f7881e
wangyuanfei <979433528@qq.com>
2018-05-07 11:28:57 +0800

    first push

--- TestClosure/ViewController.swift
+++ TestClosure/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(funcWhichNeedAParaFunc(str: "gggg", para: paraFunc  )) // gggg21
+        print(funcWhichNeedAParaFunc(str: "xxxx", para: DDTest.testParaFunc))//xxxx23
+        let result = funcWhichNeedAParaFunc(str: "ssss") { (paraStr) -> String in
+            print(paraStr)//ssss2
+            return paraStr + "4"
+        }
+        print(result)//ssss24
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func funcWhichNeedAParaFunc(str : String , para : (String)->String) -> String {
+        return para(str + "2")
+    }
+    
+    func paraFunc(string:String ) -> String {
+        return string + "1"
+    }
+}
 
+class DDTest {
+    static func testParaFunc(string:String ) -> String {
+        return string + "3"
+    }
 }
 
